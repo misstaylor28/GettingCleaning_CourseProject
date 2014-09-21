c4a0e3960c3875e80b2b897746ad6d14c2688cf7
misstaylor28 <taylor.portia@gmail.com>
2014-09-21 19:37:38 -0400

    This is my final submission. I was not able to complete Section 5

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,8 @@
+library(plyr)
+library(dplyr)
+library(stringr)
+library(data.table)
+
 features <- read.table("./features.txt",
                        col.names= c("number", "feature"), 
                        fill=FALSE, 
@@ -27,11 +32,21 @@ y_test <- read.table("./test/y_test.txt",
                       col.names = "label",
                       fill=FALSE,
                       strip.white=TRUE)
+sub_train <- read.table("./train/subject_train.txt",
+                        col.names = "subject",
+                        fill = FALSE,
+                        strip.white = TRUE)
+
+sub_test <- read.table("./test/subject_test.txt",
+                        col.names = "subject",
+                        fill = FALSE,
+                        strip.white = TRUE)
 
 ## 1.  Merges the training and the test sets to create one data set
 mergedData <- tbl_df(merge(X_train, X_test, all=TRUE))
-labels <- append(y_train, y_test)
-mergedData <- cbind(mergedData, labels)
+labels <- rbind(y_train, y_test)
+subjects <- rbind(sub_train, sub_test)
+labeledData <- cbind(subjects, mergedData, labels)
 
 ## 2.  Extracts only the measurements on the mean and standard deviation for 
 ##     each measurement. 
@@ -39,8 +54,8 @@ mergedData <- cbind(mergedData, labels)
 meanMeasures <- grep("mean", features[,2])
 stdMeasures <- grep("std", features[,2])
 keep <- sort(append(meanMeasures,stdMeasures))
-keep <- append(keep, length(mergedData))
-subset <- select(mergedData, keep)
+keep <- append(keep, length(labeledData))
+subset <- select(labeledData, keep)
 
 ## 3.  Uses descriptive activity names to name the activities in the data set
 revalue(as.character(subset$label), c("1" = "WALKING", "2" = "WALKING_UPSTAIRS",
@@ -48,9 +63,15 @@ revalue(as.character(subset$label), c("1" = "WALKING", "2" = "WALKING_UPSTAIRS",
                                       "5" = "STANDING", "6" = "LAYING") )
 
 ## 4.  Appropriately labels the data set with descriptive variable names
+subColNames <- names(subset)
+# Remove non alpha-numeric characters
+newColNames <- str_replace_all(subColNames, "[[:punct:]]"," ")
+setnames(subset, old = subColNames, new = newColNames)
 
 ## 5.  From the data set in step 4, creates a second, independent tidy data set with the average of each 
 ##     variable for each activity and each subject.  
+by_subject <- group_by(subset, subject, label) %>%
+        summarise_each(funs(mean))
 
-write.table(tidyData, file = "tidyData.txt", row.names = FALSE)
+write.table(by_subject, file = "tidyData.txt", sep=",",row.names = FALSE)
 
